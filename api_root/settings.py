51c402b2d94deea61410c57bc78bfdc6c64146e8
root <kalebiisaa517@gmail.com>
2026-05-01 18:27:27 -0300

    atualizacoes

--- api_root/settings.py
+++ api_root/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['kalebegledes.pythonanywhere.com']
 
